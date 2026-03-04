1af3c8d15b3af598b32e5abe1cbdd87724c507fc
Maxi Souto <max.xies12@gmail.com>
2026-03-04 14:32:20 -0300

    Actualizado combatant con los metodos de daño

--- src/Combatant.java
+++ src/Combatant.java
@@ -26,6 +26,80 @@ public abstract class Combatant {
         this.speed = speed;
     }
  
+    public boolean isAlive() {
+        return currentHP > 0;
+    }
+
+    public int takeDamage(int damage) {
+        int realDamage = (damage - this.defense);
+
+        if (realDamage <0) { 
+           realDamage = 0;
+        }
+       
+        this.currentHP -= realDamage;
+        if (this.currentHP < 0) {
+            this.currentHP = 0;
+        }
     
+        return damage;
+    }
+
+    
+    public int attack(Combatant target) {
+        return target.takeDamage(this.attack);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public int getMaxMP() {
+        return maxMP;
+    }
+
+    public void setMaxMP(int maxMP) {
+        this.maxMP = maxMP;
+    }
+
+    public int getCurrentMP() {
+        return currentMP;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+
+
 }
 
